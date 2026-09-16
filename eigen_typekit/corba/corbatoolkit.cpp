bd26355f680d0cd1fbe814c89374dd5e19c6431a
liufang <liufang_robot@outlook.com>
2026-09-16 08:29:27 +0800

    fix(eigen_typekit): use qualified Eigen type names and preserve CPF rows

--- eigen_typekit/corba/corbatoolkit.cpp
+++ eigen_typekit/corba/corbatoolkit.cpp
@@ -36,9 +36,9 @@ namespace Eigen
 
             bool CorbaEigenPlugin::registerTransport(std::string name, TypeInfo* ti)
             {
-                if ( name == "eigen_vector" )
+                if ( name == "/Eigen/VectorXd" )
                     return ti->addProtocol(ORO_CORBA_PROTOCOL_ID, new CorbaTemplateProtocol< Eigen::VectorXd >() );
-                if ( name == "eigen_matrix" )
+                if ( name == "/Eigen/MatrixXd" )
                     return ti->addProtocol(ORO_CORBA_PROTOCOL_ID, new CorbaTemplateProtocol< Eigen::MatrixXd >() );
                 return false;
             }
